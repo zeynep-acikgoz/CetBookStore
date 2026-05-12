2a3f1b1f519855f46be761f89f2f15fc3847c191
Zeynep Açıkgöz <zynplazim@gmail.com>
2026-05-12 22:17:30 +0300

    Improve image management and edit page usability - Added 1024px auto-resizing and old file cleanup logic. - Fixed price input fields to prevent manual cleanup of trailing zeros on edit screen. - Enabled real-time image preview on the edit page. - Standardized book card visuals across the home page.

--- CetBookStore/Controllers/BooksController.cs
+++ CetBookStore/Controllers/BooksController.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -8,6 +8,8 @@ using Microsoft.EntityFrameworkCore;
 using CetBookStore.Data;
 using CetBookStore.Models;
 using Microsoft.AspNetCore.Authorization;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
 
 namespace CetBookStore.Controllers
 {
@@ -22,53 +24,40 @@ namespace CetBookStore.Controllers
             _hostEnvironment = hostEnvironment;
         }
 
-        // GET: Books
         public async Task<IActionResult> Index()
         {
             var applicationDbContext = _context.Books.Include(b => b.Category);
             return View(await applicationDbContext.ToListAsync());
         }
 
-        // GET: Books/Details/5
         public async Task<IActionResult> Details(int? id)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
+            if (id == null) return NotFound();
 
             var book = await _context.Books
                 .Include(b => b.Category)
                 .Include(b => b.Comments)
                 .FirstOrDefaultAsync(m => m.Id == id);
-            if (book == null)
-            {
-                return NotFound();
-            }
+                
+            if (book == null) return NotFound();
 
             return View(book);
         }
 
-        // GET: Books/Create
         public IActionResult Create()
         {
             ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Name");
             return View();
         }
 
-        // POST: Books/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Id,Title,Description,Author,Publisher,PageCount,Price,IsInSale,PreviousPrice,PublicationDate,CategoryId,ImageFile")] Book book)
         {
             if (ModelState.IsValid)
             {
-
                 if (book.ImageFile != null)
                 {
-
                     var fileExtension = Path.GetExtension(book.ImageFile.FileName);
                     var newFileName = Guid.NewGuid().ToString("N") + fileExtension;
 
@@ -76,66 +65,77 @@ namespace CetBookStore.Controllers
                         new FileStream(Path.Combine(_hostEnvironment.WebRootPath,
                         "images", newFileName),
                         FileMode.Create));
+                        
                     book.ImageUrl = newFileName;  
                     _context.Add(book);
-                     await _context.SaveChangesAsync();
-                     return RedirectToAction(nameof(Index));
-                } else                 {
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                } 
+                else                 
+                {
                     ModelState.AddModelError("ImageFile", "Please upload an image.");
-
                 }
-              
             }
             ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Name", book.CategoryId);
             return View(book);
         }
 
-        // GET: Books/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
+            if (id == null) return NotFound();
 
             var book = await _context.Books.FindAsync(id);
-            if (book == null)
-            {
-                return NotFound();
-            }
+            if (book == null) return NotFound();
+            
             ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Name", book.CategoryId);
             return View(book);
         }
+
         [Authorize]
-        // POST: Books/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Description,Author,Publisher,PageCount,Price,IsInSale,PreviousPrice,PublicationDate,CreatedDate,CategoryId")] Book book)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Description,Author,Publisher,PageCount,Price,IsInSale,PreviousPrice,PublicationDate,CreatedDate,CategoryId,ImageUrl,ImageFile")] Book book)
         {
-            if (id != book.Id)
-            {
-                return NotFound();
-            }
+            if (id != book.Id) return NotFound();
 
             if (ModelState.IsValid)
             {
                 try
                 {
+                    if (book.ImageFile != null)
+                    {
+                        if (!string.IsNullOrEmpty(book.ImageUrl))
+                        {
+                            var oldImagePath = Path.Combine(_hostEnvironment.WebRootPath, "images", book.ImageUrl);
+                            if (System.IO.File.Exists(oldImagePath))
+                            {
+                                System.IO.File.Delete(oldImagePath);
+                            }
+                        }
+
+                        var fileExtension = Path.GetExtension(book.ImageFile.FileName);
+                        var newFileName = Guid.NewGuid().ToString("N") + fileExtension;
+                        var savePath = Path.Combine(_hostEnvironment.WebRootPath, "images", newFileName);
+
+                        using (var image = await Image.LoadAsync(book.ImageFile.OpenReadStream()))
+                        {
+                            if (image.Width > 1024)
+                            {
+                                image.Mutate(x => x.Resize(1024, 0));
+                            }
+                            await image.SaveAsync(savePath); 
+                        }
+
+                        book.ImageUrl = newFileName;
+                    }
+
                     _context.Update(book);
                     await _context.SaveChangesAsync();
                 }
                 catch (DbUpdateConcurrencyException)
                 {
-                    if (!BookExists(book.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
+                    if (!BookExists(book.Id)) return NotFound();
+                    else throw;
                 }
                 return RedirectToAction(nameof(Index));
             }
@@ -143,26 +143,19 @@ namespace CetBookStore.Controllers
             return View(book);
         }
 
-        // GET: Books/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
+            if (id == null) return NotFound();
 
             var book = await _context.Books
                 .Include(b => b.Category)
                 .FirstOrDefaultAsync(m => m.Id == id);
-            if (book == null)
-            {
-                return NotFound();
-            }
+                
+            if (book == null) return NotFound();
 
             return View(book);
         }
 
-        // POST: Books/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
@@ -185,45 +178,25 @@ namespace CetBookStore.Controllers
         [Authorize]
         public async Task<IActionResult> Buy(int id, int count=1)
         {
-            if(count<1)
-            {
-                return BadRequest();
-            }
-            var book = await _context.Books.FindAsync(id);
-            if(book==null)
-            {                
-                return NotFound();
-            }
+            if(count < 1) return BadRequest();
             
-           // Sale sale = new Sale();
-           // sale.CetUserId =_context.Users.FirstOrDefault(u => u.UserName == User.Identity.Name)?.Id;
-            //sale.BookId = id;
-
-           // sale.TotalPrice = book.Price*count;
-           // sale.SalesDate = DateTime.Now;
-           // sale.Count = count;
-           // _context.Sales.Add(sale);
-           // await _context.SaveChangesAsync();
+            var book = await _context.Books.FindAsync(id);
+            if(book == null) return NotFound();
+            
             return RedirectToAction( "Index", "Home");
-
         }
+
         public async Task<IActionResult> CommentCreate([Bind("Id,UserName,Content,BookId")] Comment comment)
         {
-
             comment.CreatedDate = DateTime.Now;
 
             if (HttpContext.User.Identity.IsAuthenticated)
             {
                 comment.UserName = HttpContext.User.Identity.Name;
-
             }
-           
 
-           
-                _context.Add(comment);
-                await _context.SaveChangesAsync();
-                
-            
+            _context.Add(comment);
+            await _context.SaveChangesAsync();
 
             return RedirectToAction(nameof(Details),new { id = comment.BookId });
         }
